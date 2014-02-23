9025d925d535d292c1ec9d0d81efa8a7c7988bb9
samuel berthe <dev@samuel-berthe.fr>
2014-02-23 10:35:12 +0100

    coucou

--- config/application.rb
+++ config/application.rb
@@ -61,9 +61,6 @@ module Load
 
 
     config.action_dispatch.default_headers = {
-      'X-Frame-Options' => 'SAMEORIGIN',
-      'X-XSS-Protection' => '1; mode=block',
-      'X-Content-Type-Options' => 'nosniff',
       'Access-Control-Allow-Origin' => '*'
     }
 
