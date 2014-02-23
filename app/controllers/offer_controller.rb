f848c10b03784a8fa5e3dc113780b448740a80f9
samuel berthe <dev@samuel-berthe.fr>
2014-02-23 10:14:15 +0100

    en proood !

--- app/controllers/offer_controller.rb
+++ app/controllers/offer_controller.rb
@@ -24,4 +24,18 @@ class OfferController < ApplicationController
     offers = Offer.where("title LIKE ? OR description LIKE ?", q, q)
     render :json => offers
   end
+
+
+
+  def allow_cors
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+    headers["Access-Control-Allow-Headers"] =
+      %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+
+    head(:ok) if request.request_method == "OPTIONS"
+    # or, render text: ''
+    # if that's more your style
+  end
+
 end
